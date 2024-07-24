9e34dd2a13a3bcc95fc3f33a35f40554fb14cdde
Arnav Mehta <arnavdatt@gmail.com>
2024-07-24 17:01:31 +0100

    Added SDL window
    Also removed platform window layer

--- Core/Application/Application.h
+++ Core/Application/Application.h
@@ -1,17 +1,25 @@
 #pragma once
 #include <Core/Common.h>
-#include <Core/Platform/Window.h>
 #include <Core/Logging/Log.h>
+
+struct SDL_Window;
 
 namespace CGL::Core
 {
 	CGL_DECLARE_LOG_CATEGORY(CoreApp);
 
-	class Application : public Window<PlatformWindow>
+	class Application
 	{
-	protected:
-		using Super = Window<PlatformWindow>;
-
+	public:
+		struct Config
+		{
+			u32 Width = 1280;
+			u32 Height = 720;
+			bool Fullscreen = false;
+			bool IsVsync = true;
+			
+			// Choose RHI here
+		};
 	public:
 		Application();
 		~Application() = default;
@@ -24,9 +32,9 @@ namespace CGL::Core
 		virtual void OnRender();
 		virtual void OnShutdown();
 		virtual void OnResize(u32 width, u32 height);
-		virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) override;
 
 	protected:
 		bool m_isRunning;
+		SDL_Window* m_window;
 	};
 }
