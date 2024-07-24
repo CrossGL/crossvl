92a0cd4539e3f8e8dce9342fda06c819c09e2680
Arnav Mehta <arnavdatt@gmail.com>
2024-07-24 17:10:45 +0100

    Remove win32 utils
    Removing this since we are now using SSL for windowing

--- Core/Platform/Win32/Win32Utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Core/Common.h>
-#include <Windows.h>
-
-namespace CGL::Core::Platform::Win32::Utils
-{
-	void GetAdjustedWindowSize(DWORD style, DWORD exStyle, u32& width, u32& height);
-	void GetDesktopCentreForWindow(u32 width, u32 height, u32& x, u32& y);
-}
