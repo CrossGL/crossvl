7428f8c4e3d09679673cf5a2109d2a7301440b78
abhayMore <abhay7more@gmail.com>
2024-07-27 14:53:27 +0000

    Implemented RHI for OpenGL

---
+++ Core/Graphics/RHI/OPENGL/OPENGLRenderer.cpp
@@ -0,0 +1,45 @@
+#include <Core/Graphics/Renderer.h>
+#include <Core/Graphics/RHI/OPENGL/OPENGLRendererImpl.h>
+
+namespace CGL::Graphics
+{
+    #ifdef CGL_RHI_OPENGL
+    void Renderer::Constructor_OPENGL(SDL_Window* window)
+    {
+        this->m_impl = new OPENGLRendererImpl(window);
+
+        CGL_LOG(Renderer, Info, "OpenGL Renderer Initialized");
+    }
+
+    void Renderer::Destructor_OPENGL()
+    {
+        delete static_cast<OPENGLRendererImpl*>(m_impl);
+        m_impl = nullptr;
+
+        CGL_LOG(Renderer, Info, "OPENGL Renderer Destroyed");
+    }
+
+    OPENGLRendererImpl* Renderer::GetImpl() const
+    {
+        assert(GetAPI() == RHIType::OpenGL);
+        return static_cast<OPENGLRendererImpl*>(m_impl);
+    }
+
+    void Renderer::BeginFrame_OPENGL()
+    {
+        glClearDepth(1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+    
+    void Renderer::EndFrame_OPENGL()
+    { 
+        GetImpl()->SwapWindow();
+    }
+
+    void Renderer::Resize_OPENGL(u32 width, u32 height)
+	{
+	    glViewport(0, 0, width, height);
+	}
+
+    #endif // CGL_RHI_OPENGL
+}
